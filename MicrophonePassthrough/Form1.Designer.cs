74ed6f8d7020af9d4ccb12234e8819cee054bdf8
Adam <adam@lynam.co.nz>
2014-10-03 20:56:22 +0100

    Initial commit of code.

---
+++ MicrophonePassthrough/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MicrophonePassthrough
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passthroughToggleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // passthroughToggleButton
+            // 
+            this.passthroughToggleButton.Location = new System.Drawing.Point(12, 12);
+            this.passthroughToggleButton.Name = "passthroughToggleButton";
+            this.passthroughToggleButton.Size = new System.Drawing.Size(260, 85);
+            this.passthroughToggleButton.TabIndex = 0;
+            this.passthroughToggleButton.Text = "Start Passthrough";
+            this.passthroughToggleButton.UseVisualStyleBackColor = true;
+            this.passthroughToggleButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 110);
+            this.Controls.Add(this.passthroughToggleButton);
+            this.Name = "Form1";
+            this.Text = "Microphone Passthrough";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button passthroughToggleButton;
+    }
+}
+
---
+++ MicrophonePassthrough/Form1.cs
@@ -0,0 +1,73 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicrophonePassthrough
+{
+    public partial class Form1 : Form
+    {
+        private WaveIn waveIn;
+        private WaveOut waveOut;
+        private BufferedWaveProvider waveStream;
+
+        private bool passthroughActive;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.passthroughActive = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!this.passthroughActive)
+            {
+                this.waveStream = new BufferedWaveProvider(new WaveFormat(64000, 1));
+                //Debug.WriteLine(waveStream.BufferLength);
+                //Debug.WriteLine(waveStream.BufferDuration);
+                waveStream.BufferDuration = TimeSpan.FromSeconds(0.2);
+                this.waveIn = new WaveIn();
+                this.waveOut = new WaveOut();
+                this.waveIn.WaveFormat = new WaveFormat(64000, 1);
+                this.waveIn.BufferMilliseconds = 5;
+                this.waveIn.DataAvailable += OnDataAvailable;
+                ISampleProvider sampleProvider = new SampleChannel(this.waveStream);
+                this.waveOut.Init(sampleProvider);
+                this.waveIn.StartRecording();
+                waveOut.Play();
+                this.passthroughActive = true;
+                passthroughToggleButton.Text = "Stop Passthrough";
+            }
+            else
+            {
+                this.waveIn.StopRecording();
+                this.passthroughActive = false;
+                passthroughToggleButton.Text = "Start Passthrough";
+            }
+        }
+
+        void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                //Debug.WriteLine("Data Available");
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(OnDataAvailable), sender, e);
+            }
+            else
+            {
+                //Debug.WriteLine("Flushing Data Available");
+                waveStream.AddSamples(e.Buffer, 0, e.BytesRecorded);
+            }
+        }
+    }
+}
